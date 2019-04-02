58b15b09b32801d4669a27c322a6dd4369b00319
Alice Yang <42975566+Picsesalg@users.noreply.github.com>
2019-04-02 11:33:37 -0400

    #LogIn - Log In page placeholders done. Navigation needs to be done

--- app/src/main/java/com/example/ufree/MainActivity.java
+++ app/src/main/java/com/example/ufree/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ufree;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,7 +82,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            // TODO remove
+            Intent intent = new Intent(this, LogIn.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
