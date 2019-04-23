36d30aacea062dcbaa1fd6c64a0037d2123dbe98
Alice Yang <42975566+Picsesalg@users.noreply.github.com>
2019-04-23 15:54:54 -0400

    #34

--- app/src/main/java/com/example/ufree/FriendRequestViewHolder.java
+++ app/src/main/java/com/example/ufree/FriendRequestViewHolder.java
@@ -2,8 +2,6 @@ package com.example.ufree;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.Image;
-import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
@@ -11,11 +9,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class FriendRequestViewHolder extends RecyclerView.ViewHolder
     implements View.OnClickListener {
@@ -36,12 +29,24 @@ public class FriendRequestViewHolder extends RecyclerView.ViewHolder
         accept = (ImageView) itemView.findViewById(R.id.accept);
         reject = (ImageView) itemView.findViewById(R.id.reject);
 
+        constraintLayout.setOnClickListener(this);
         accept.setOnClickListener(this);
         reject.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+
+        // If the entire row is clicked
+        if (v.getId() == constraintLayout.getId()) {
+            Toast.makeText(v.getContext(), "Row pressed: " + String.valueOf(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+            String e = email.getText().toString();
+            Intent i = new Intent(v.getContext(), FriendProfileActivity.class);
+            i.putExtra("email", e);
+            v.getContext().startActivity(i);
+        }
+
+        // Accept button
         if (v.getId() == accept.getId()) {
             AlertDialog.Builder alert = new AlertDialog.Builder(v.getContext());
             alert.setTitle("Add Friend");
@@ -61,10 +66,11 @@ public class FriendRequestViewHolder extends RecyclerView.ViewHolder
                     dialog.dismiss();
                 }
             });
-
             alert.show();
             return;
         }
+
+        // Reject button
         if (v.getId() == reject.getId()) {
             AlertDialog.Builder alert = new AlertDialog.Builder(v.getContext());
             alert.setTitle("Delete Friend Request");
@@ -84,7 +90,6 @@ public class FriendRequestViewHolder extends RecyclerView.ViewHolder
                     dialog.dismiss();
                 }
             });
-
             alert.show();
             return;
         }
